88d091baf72b47d610a2bf88eb0ad5befd48d39b
osama123098 <osamashaikhansari@gmail.com>
2023-02-26 04:55:30 -0500

    array_easy

--- Contain_duplicate.js
+++ Contain_duplicate.js
@@ -1,10 +1,14 @@
 // Duplicate in array
 arr = [1,2,3,4,5,6,7,8,9,1]
 function duplicate(a){
+    arr  = []
     for(i=0;i<a.length;i++){
-
-        for(j=0;j<a.length;j++){
-               
+        if(!(a[i]in arr)){
+            arr.push(a[i])
         }
+        else
+        return true
     }
+    return false
 }
+console.log(duplicate(arr))
